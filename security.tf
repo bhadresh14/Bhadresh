fa882715641fcc35d5b53fdf4df6581cb1f77f00
Bhadresh <bhadreshwarabhadresh.com>
2025-06-20 12:44:25 +0530

    Add security group for web and SSH

---
+++ security.tf
@@ -0,0 +1,30 @@
+resource "aws_security_group" "web_sg" {
+  name        = "web-security-group"
+  description = "Allow web and SSH access"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "web-sg"
+  }
+}
